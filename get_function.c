dcd80655268618b3cc24476f0899c62861259544
Nick Nyanjui <nicknyanjui@gmail.com>
2022-05-22 14:03:32 -0700

    Update program get_function.c; betty styling

--- get_function.c
+++ get_function.c
@@ -9,20 +9,20 @@ int (*get_function(char c))(va_list a, char *s, int *index)
 	int c1;
 	choose l[] = {
 		{'c', print_char},
-	{'s', print_string}, 
-	{'%', print_percent}, 
-	{'i', print_integer},
-		{'d', print_integer}, 
-	{'b', print_binary}, 
-	{'u', print_unsigned}, 
-	{'o', print_octal},
-		{'x', print_hex}, 
-	{'X', print_HEX}, 
-	{'S', print_special_char}, 
-	{'R', print_Rot13},
-		{'r', print_reverse}, 
-	{'p', print_address}, 
-	{'\0', NULL}
+		{'s', print_string},
+		{'%', print_percent},
+		{'i', print_integer},
+		{'d', print_integer},
+		{'b', print_binary},
+		{'u', print_unsigned},
+		{'o', print_octal},
+		{'x', print_hex},
+		{'X', print_HEX},
+		{'S', print_special_char},
+		{'R', print_Rot13},
+		{'r', print_reverse},
+		{'p', print_address},
+		{'\0', NULL}
 	};
 	for (c1 = 0; l[c1].c != '\0'; c1++)
 	{
